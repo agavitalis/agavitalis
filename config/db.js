e9a98dcbea14936dc395286cfad9467d99cbc16f
Ogbonna <agavitalisogbonna@gmail.com>
2020-12-30 11:33:42 +0100

    updated the db

--- config/db.js
+++ config/db.js
@@ -4,7 +4,8 @@ var mongoose = require('mongoose')
 //mongoose.connect('mongodb://localhost:27017/portfolio',{useUnifiedTopology: true,useNewUrlParser: true});
 
 //before going live, uncomment this connection
-mongoose.connect('mongodb://portfolio:Chidiebere94@ds131784.mlab.com:31784/portfolio', {useUnifiedTopology: true,useNewUrlParser: true});
+
+mongoose.connect('mongodb+srv://agavitalis:Chidiebere_94@@cluster0.uxftt.mongodb.net/portfolio?retryWrites=true&w=majority', {useUnifiedTopology: true,useNewUrlParser: true});
 
 //lets check if the connection was not successful
 mongoose.connection.on('error', console.error.bind(console, 'connection error'))
